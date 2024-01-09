ea361b9d8f06b6421e7163bf4ec43fb2b6e84aed
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2024-01-09 17:22:19 -0500

    auth-btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { AuthButton } from "@/app/components/auto-button";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+//con estos dos componentes son necesarios en Nextjs
+//Para hacer la conexion con la base de datos
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: post } = await supabase.from("post").select();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hola XXXXXXXXX 🙋🏻‍♂️👋🏻
+      <AuthButton />
+      <pre>{JSON.stringify(post, null, 2)}</pre>
     </main>
-  )
+  );
 }
