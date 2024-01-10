b22f07dfaf7fad22625d0e84554e51d6a9dc2b0e
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2024-01-10 13:27:10 -0500

    add information cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { AuthButtonServer } from "./components/auth-button-server";
 import { redirect } from "next/navigation";
+import PostCard from "./components/post.card";
 //con estos dos componentes son necesarios en Nextjs
 //Para hacer la conexion con la base de datos
 export default async function Home() {
@@ -16,12 +17,29 @@ export default async function Home() {
 
   const { data: post } = await supabase
     .from("post")
-    .select("*, users(name, avatar_url, user_name)");
+    .select("*, user:users(name, avatar_url, user_name)");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButtonServer /> Hola XXXXXXXXX 🙋🏻‍♂️👋🏻
-      <pre>{JSON.stringify(post, null, 2)}</pre>
+      <AuthButtonServer />
+
+      {post?.map((post) => {
+        const { id, user, content } = post;
+        const {
+          user_name: userName,
+          name: userFullName,
+          avatar_url: avatarUrl,
+        } = user;
+        return (
+          <PostCard
+            avatarUrl={avatarUrl}
+            content={content}
+            key={id}
+            userFullName={userFullName}
+            userName={userName}
+          />
+        );
+      })}
     </main>
   );
 }
